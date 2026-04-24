d8285eb878323aef17203b9a815a1c35d6e2d1b1
Jared Smoley <jared.smoley@servicenow.com>
2026-04-24 12:46:42 -0700

    fix: GodMode now sets disableDamage=true for real invulnerability
    - onEnable sets capabilities.disableDamage=true and health to max
    - onUpdate enforces both every tick so damage cannot reduce health
    - onDisable restores disableDamage=false cleanly

--- src/main/java/proclient/module/player/GodMode.java
+++ src/main/java/proclient/module/player/GodMode.java
@@ -3,7 +3,6 @@ package proclient.module.player;
 import proclient.module.Category;
 import proclient.module.Module;
 import net.lax1dude.eaglercraft.v1_8.internal.KeyboardConstants;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGameOver;
 
 public class GodMode extends Module {
@@ -11,15 +10,31 @@ public class GodMode extends Module {
         super("GodMode", KeyboardConstants.KEY_NONE, Category.PLAYER);
     }
 
-    public void onUpdate() {
-        if(this.isToggled()) {
-            if(mc.thePlayer.getHealth() <= 0) {
-                mc.thePlayer.setHealth(mc.thePlayer.getMaxHealth());
-                if(mc.currentScreen instanceof GuiGameOver) {
-                    mc.currentScreen = null;
-                }
-            }
+    @Override
+    public void onEnable() {
+        if (mc.thePlayer == null) return;
+        mc.thePlayer.capabilities.disableDamage = true;
+        mc.thePlayer.setHealth(mc.thePlayer.getMaxHealth());
+        super.onEnable();
     }
-}
 
+    @Override
+    public void onDisable() {
+        if (mc.thePlayer != null) {
+            mc.thePlayer.capabilities.disableDamage = false;
+        }
+        super.onDisable();
+    }
+
+    @Override
+    public void onUpdate() {
+        if (!this.isToggled() || mc.thePlayer == null) return;
+        mc.thePlayer.capabilities.disableDamage = true;
+        if (mc.thePlayer.getHealth() < mc.thePlayer.getMaxHealth()) {
+            mc.thePlayer.setHealth(mc.thePlayer.getMaxHealth());
+        }
+        if (mc.currentScreen instanceof GuiGameOver) {
+            mc.currentScreen = null;
+        }
+    }
 }
